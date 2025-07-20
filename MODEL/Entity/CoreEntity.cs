bbf81d00953a19d574a4289f418faa50c77c8dc5
Alperen <sonqyuncu@gmail.com>
2025-07-20 18:06:17 +0300

    Yeni dosyalar ve değişiklikler eklendi

--- MODEL/Entity/CoreEntity.cs
+++ MODEL/Entity/CoreEntity.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,7 @@ namespace MODEL.Entity
 {
     internal class CoreEntity
     {
+        public int İd { get; set; }
+        public DateTime CreatedAt { get; set; }
     }
 }
