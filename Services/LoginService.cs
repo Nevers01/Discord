32f3fd1fcb93d3f1041c87aeeb6d0fcc5d033409
Nevers01 <sonqyuncu@gmail.com>
2025-07-24 22:14:22 +0300

    Yeni güncellemeler eklendi

--- Services/LoginService.cs
+++ Services/LoginService.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Services.İnterface;
 
 namespace Services
 {
-    internal class LoginService
+    public class LoginService : ILoginService
     {
+        public void check(string Username, string PasswordHash)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
